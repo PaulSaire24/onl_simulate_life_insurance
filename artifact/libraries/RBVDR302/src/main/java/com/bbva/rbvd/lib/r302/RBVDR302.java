3dc8c95f284cfed1295669f570ef45cfdb52bb90
augusto.martinez <augusto.martinez@bbva.com>
2023-04-08 22:37:27 -0500

    PERAR-22132: for build 4

--- artifact/libraries/RBVDR302/src/main/java/com/bbva/rbvd/lib/r302/RBVDR302.java
+++ artifact/libraries/RBVDR302/src/main/java/com/bbva/rbvd/lib/r302/RBVDR302.java
@@ -1,11 +1,13 @@
 package com.bbva.rbvd.lib.r302;
 
+
 import com.bbva.rbvd.dto.lifeinsrc.simulation.LifeSimulationDTO;
 
 /**
  * The  interface RBVDR302 class...
  */
 public interface RBVDR302 {
+
 
 	/**
 	 * The execute method...
