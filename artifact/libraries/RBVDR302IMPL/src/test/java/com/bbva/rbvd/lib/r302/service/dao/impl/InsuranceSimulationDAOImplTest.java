d4979587a926cdca0fe9f3e340cacaae84999bc7
Aaron <carlos.carrillo.delgado@bbva.com>
2023-07-21 15:38:44 -0500

    test: PERAR-23816 PERAR-24210 unit test dynamic flow with modify quotation service

--- artifact/libraries/RBVDR302IMPL/src/test/java/com/bbva/rbvd/lib/r302/service/dao/impl/InsuranceSimulationDAOImplTest.java
+++ artifact/libraries/RBVDR302IMPL/src/test/java/com/bbva/rbvd/lib/r302/service/dao/impl/InsuranceSimulationDAOImplTest.java
@@ -2,13 +2,7 @@ package com.bbva.rbvd.lib.r302.service.dao.impl;
 
 import com.bbva.apx.exception.business.BusinessException;
 import com.bbva.pisd.lib.r350.PISDR350;
-<<<<<<< HEAD
-import com.bbva.rbvd.dto.lifeinsrc.utils.RBVDErrors;
 import com.bbva.rbvd.dto.lifeinsrc.utils.RBVDProperties;
-import com.bbva.rbvd.dto.lifeinsrc.utils.RBVDValidation;
-=======
-import com.bbva.rbvd.dto.lifeinsrc.utils.RBVDProperties;
->>>>>>> b173725e97713327a22a9240c3c5fc25f80b3dca
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,17 +10,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-<<<<<<< HEAD
-import java.io.IOException;
-import java.util.Map;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-=======
 
-
-import static org.mockito.Mockito.mock;
->>>>>>> b173725e97713327a22a9240c3c5fc25f80b3dca
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -36,7 +22,7 @@ import static org.mockito.Mockito.mock;
         "classpath:/META-INF/spring/RBVDR302-arc-test.xml" })
 
 public class InsuranceSimulationDAOImplTest extends TestCase {
-<<<<<<< HEAD
+
     private PISDR350 pisdR350;
     private InsuranceSimulationDAOImpl insuranceSimulation;
     @Before
@@ -44,19 +30,7 @@ public class InsuranceSimulationDAOImplTest extends TestCase {
         pisdR350 = mock(PISDR350.class);
         insuranceSimulation = new InsuranceSimulationDAOImpl(pisdR350);
     }
-    @Test(expected = BusinessException.class)
-    public void getInsertInsuranceSimulationFailCase() {
 
-=======
-
-    private PISDR350 pisdR350;
-    private InsuranceSimulationDAOImpl insuranceSimulation;
-
-    @Before
-    public void setUp() {
-        pisdR350 = mock(PISDR350.class);
-        insuranceSimulation = new InsuranceSimulationDAOImpl(pisdR350);
-    }
 
     /*@Test
     public void InsuranceSimulationDAOImplNotOne() {
@@ -74,7 +48,6 @@ public class InsuranceSimulationDAOImplTest extends TestCase {
 
     @Test(expected = BusinessException.class)
     public void getInsertInsuranceSimulationFailCase() {
->>>>>>> b173725e97713327a22a9240c3c5fc25f80b3dca
         insuranceSimulation.getInsertInsuranceSimulation(null);
         Mockito.verify(pisdR350, Mockito.atLeastOnce()).executeInsertSingleRow(RBVDProperties.QUERY_INSERT_INSURANCE_SIMULATION.getValue(),null);
     }
