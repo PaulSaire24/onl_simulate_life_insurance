72ec2813b7aa915c0fadd184033ed8cedbde8fab
Aaron <carlos.carrillo.delgado@bbva.com>
2023-08-01 09:22:55 -0500

    feat: PERAR-23816 PERAR-24338 Validate plan in request with % refund

--- artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/pattern/impl/SimulationVidaDinamico.java
+++ artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/pattern/impl/SimulationVidaDinamico.java
@@ -1,6 +1,7 @@
 package com.bbva.rbvd.lib.r302.pattern.impl;
 
 import com.bbva.elara.configuration.manager.application.ApplicationConfigurationService;
+import com.bbva.rbvd.dto.lifeinsrc.commons.InsurancePlanDTO;
 import com.bbva.rbvd.dto.lifeinsrc.simulation.LifeSimulationDTO;
 import com.bbva.rbvd.lib.r301.RBVDR301;
 import com.bbva.rbvd.lib.r302.transfer.PayloadConfig;
@@ -13,6 +14,11 @@ import com.bbva.rbvd.lib.r302.util.ValidationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SimulationVidaDinamico extends SimulationDecorator{
 	private static final Logger LOGGER = LoggerFactory.getLogger(SimulationVidaDinamico.class);
 
@@ -23,6 +29,8 @@ public class SimulationVidaDinamico extends SimulationDecorator{
 	@Override
 	public LifeSimulationDTO start(LifeSimulationDTO input, RBVDR301 rbvdR301, ApplicationConfigurationService applicationConfigurationService) {
 		LOGGER.info("***** SimulationVidaDinamico - start START | input {} *****",input);
+
+		validatePlanWithRefundPercentage(input);
 
 		PayloadConfig payloadConfig = this.getPreSimulation().getConfig(input);
 
@@ -45,6 +53,25 @@ public class SimulationVidaDinamico extends SimulationDecorator{
 		LOGGER.info("***** RBVDR302Impl - SimulationVidaDinamico.start() END *****");
 
 		return payloadStore.getResponse();
+	}
+
+	private static void validatePlanWithRefundPercentage(LifeSimulationDTO input) {
+		if(input.getListRefunds() != null){
+			BigDecimal percentage = input.getListRefunds().stream().filter(refundsDTO -> refundsDTO.getUnit().getUnitType().equals("PERCENTAGE")).map(refundsDTO -> refundsDTO.getUnit().getPercentage()).collect(Collectors.toList()).get(0);
+			if(percentage.compareTo(BigDecimal.ZERO) == 0){
+				InsurancePlanDTO plan01 = new InsurancePlanDTO();
+				plan01.setId("01");
+				List<InsurancePlanDTO> plans = new ArrayList<>();
+				plans.add(plan01);
+				input.getProduct().setPlans(plans);
+			}else{
+				InsurancePlanDTO plan02 = new InsurancePlanDTO();
+				plan02.setId("02");
+				List<InsurancePlanDTO> plans = new ArrayList<>();
+				plans.add(plan02);
+				input.getProduct().setPlans(plans);
+			}
+		}
 	}
 
 
