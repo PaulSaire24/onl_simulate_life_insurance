0304a5682855d585511c55124324135ddd5cce3d
Aaron <carlos.carrillo.delgado@bbva.com>
2023-08-17 15:22:49 -0500

    fix: PERAR-24473 PERAR-24551 Resolve error in Sonar unit test financing querterly and bimonthly

--- artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/util/ConstantsUtil.java
+++ artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/util/ConstantsUtil.java
@@ -2,9 +2,6 @@ package com.bbva.rbvd.lib.r302.util;
 
 public class ConstantsUtil {
 
-    private ConstantsUtil(){}
-
-
     public static final String AMOUNT_UNIT_TYPE = "AMOUNT";
     public static final String ANNUAL_PERIOD_ID = "ANNUAL";
     public static final String ANNUAL_PERIOD_NAME = "ANUAL";
