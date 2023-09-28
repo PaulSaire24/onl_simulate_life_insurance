bd11ac572383979e269e86837b948064a31e97bc
paulkyev <paul.saire.contractor@bbva.com>
2023-09-28 14:43:51 -0500

    perf: PERAR-25016 PERAR-25185 reafactor lib remove hard-coded in credential

--- artifact/libraries/RBVDR301IMPL/src/main/java/com/bbva/rbvd/lib/r301/impl/util/Constans.java
+++ artifact/libraries/RBVDR301IMPL/src/main/java/com/bbva/rbvd/lib/r301/impl/util/Constans.java
@@ -2,7 +2,7 @@ package com.bbva.rbvd.lib.r301.impl.util;
 
 public class Constans {
     public static final String APP_NAME ="APP_NAME";
-    public static final String PASSWORD ="PASSWORD";
+    public static final String OAUTH_TOKEN ="PASSWORD";
     public static final String CRE_EXTRA_PARAMS ="CRE_EXTRA_PARAMS";
     public static final String INPUT_TEXT_SECURITY ="INPUT_TEXT_SECURITY";
 }
