2b07b09a0616dc2987dcf701bd57e3246ed94e84
paulkyev <paul.saire.contractor@bbva.com>
2023-10-17 10:43:06 -0500

    temp

--- artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/transform/bean/SimulationParticipanBean.java
+++ artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/transform/bean/SimulationParticipanBean.java
@@ -1,22 +1,77 @@
 package com.bbva.rbvd.lib.r302.transform.bean;
 
-import com.bbva.rbvd.dto.lifeinsrc.dao.SimulationParticipantDAO;
+import com.bbva.pisd.dto.insurance.aso.CustomerListASO;
+import com.bbva.rbvd.dto.lifeinsrc.dao.CommonsLifeDAO;
 import com.bbva.rbvd.dto.lifeinsrc.simulation.LifeSimulationDTO;
+import com.bbva.rbvd.lib.r302.util.ConstantsUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+
+import static com.bbva.rbvd.lib.r302.util.ConstantsUtil.EMAIL;
+import static com.bbva.rbvd.lib.r302.util.ConstantsUtil.MOBILE_NUMBER;
+import static com.bbva.rbvd.lib.r302.util.ConvertUtil.getGroupedByTypeContactDetail;
+import static com.bbva.rbvd.lib.r302.util.ValidationUtil.isBBVAClient;
+
 
 public class SimulationParticipanBean {
 
     private SimulationParticipanBean() {
     }
 
-    public static SimulationParticipantDAO createSimulationParticipant (BigDecimal insuranceSimulationId, LifeSimulationDTO insuranceSimulationDTO, String creationUser, String userAudit, BigDecimal productId){
-        SimulationParticipantDAO simulationParticipant = new SimulationParticipantDAO();
-        simulationParticipant.setInsuranceSimulationId(insuranceSimulationId);
-        simulationParticipant.setInsuranceProductId(productId);
-        simulationParticipant.setResponse(insuranceSimulationDTO);
-        simulationParticipant.setCreationUser(creationUser);
-        simulationParticipant.setUserAudit(userAudit);
-        return  simulationParticipant;
+    public static CommonsLifeDAO createSimulationParticipant (BigDecimal insuranceSimulationId, LifeSimulationDTO insuranceSimulation, String creationUser, String userAudit, BigDecimal productId, CustomerListASO customer){
+        CommonsLifeDAO commonsLife = new CommonsLifeDAO();
+        commonsLife.setInsuranceSimulationId(insuranceSimulationId);
+        commonsLife.setInsuranceProductId(productId);
+        commonsLife.setInsuredAmount(insuranceSimulation.getInsuredAmount().getAmount());
+        commonsLife.setPeriodNumber(BigDecimal.valueOf(insuranceSimulation.getTerm().getNumber()));
+        commonsLife.setPeriodType("A");
+        commonsLife.setCustomerEntryDate(new Date());
+
+        if(!CollectionUtils.isEmpty(insuranceSimulation.getListRefunds())){
+            commonsLife.setRefundPer(insuranceSimulation.getListRefunds().get(0).getUnit().getPercentage());
+            commonsLife.setCurrencyId(insuranceSimulation.getListRefunds().get(1).getUnit().getCurrency());
+            commonsLife.setTotalReturnAmount(insuranceSimulation.getListRefunds().get(1).getUnit().getAmount());
+        }
+
+        if(!CollectionUtils.isEmpty(insuranceSimulation.getParticipants())){
+            commonsLife.setInsuredId(insuranceSimulation.getParticipants().get(0).getId());
+            commonsLife.setCustomerDocumentType(insuranceSimulation.getParticipants().get(0).getIdentityDocument().getDocumentType().getId());
+            commonsLife.setInsuredCustomerName(insuranceSimulation.getParticipants().get(0).getFirstName());
+            commonsLife.setClientLastName(insuranceSimulation.getParticipants().get(0).getLastName());
+            commonsLife.setPhoneId(insuranceSimulation.getParticipants().get(0).getContactDetails().get(0).getContact().getNumber());
+            commonsLife.setCustomerBirthDate(insuranceSimulation.getParticipants().get(0).getBirthDate());
+            commonsLife.setPersonalId(insuranceSimulation.getParticipants().get(0).getIdentityDocument().getDocumentNumber());
+            commonsLife.setUserEmailPersonalDesc(insuranceSimulation.getParticipants().get(0).getContactDetails().get(1).getContact().getAddress());
+            commonsLife.setIsBbvaCustomerType(isBBVAClient(insuranceSimulation.getParticipants().get(0).getId())? ConstantsUtil.YES_S : ConstantsUtil.NO_N);
+            if(StringUtils.isEmpty(insuranceSimulation.getParticipants().get(0).getId())){
+                commonsLife.setCustomerEntryDate(null);
+            }
+        }else {
+            commonsLife.setInsuredId(insuranceSimulation.getHolder().getId());
+            commonsLife.setCustomerDocumentType(insuranceSimulation.getHolder().getIdentityDocument().getDocumentType().getId());
+            commonsLife.setInsuredCustomerName(insuranceSimulation.getHolder().getFirstName());
+            commonsLife.setClientLastName(insuranceSimulation.getHolder().getLastName());
+            commonsLife.setPhoneId(null);
+            commonsLife.setCustomerBirthDate(null);
+            commonsLife.setPersonalId(insuranceSimulation.getHolder().getIdentityDocument().getDocumentNumber());
+            commonsLife.setIsBbvaCustomerType(ConstantsUtil.YES_S);
+            commonsLife.setUserEmailPersonalDesc(null);
+            if(Objects.nonNull(customer) && StringUtils.isNotEmpty(customer.getData().get(0).getBirthData().getBirthDate())){
+                commonsLife.setCustomerBirthDate(ModifyQuotationRimac.ParseFecha(customer.getData().get(0).getBirthData().getBirthDate()));
+                Map<String, String> contactDetails = getGroupedByTypeContactDetail(customer.getData().get(0));
+                commonsLife.setUserEmailPersonalDesc(contactDetails.get(EMAIL));
+                commonsLife.setPhoneId(contactDetails.get(MOBILE_NUMBER));
+            }
+        }
+        commonsLife.setParticipantRoleId(BigDecimal.valueOf(ConstantsUtil.IS_INSURED));
+        commonsLife.setCreationUser(creationUser);
+        commonsLife.setUserAudit(userAudit);
+
+        return  commonsLife;
     }
 }
--- artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/transform/map/SimulationParticipantMap.java
+++ artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/transform/map/SimulationParticipantMap.java
@@ -1,24 +1,12 @@
 package com.bbva.rbvd.lib.r302.transform.map;
 
-import com.bbva.pisd.dto.insurance.aso.CustomerListASO;
-import com.bbva.rbvd.dto.lifeinsrc.dao.SimulationParticipantDAO;
+import com.bbva.rbvd.dto.lifeinsrc.dao.CommonsLifeDAO;
 import com.bbva.rbvd.dto.lifeinsrc.utils.RBVDProperties;
-import com.bbva.rbvd.lib.r302.util.ConstantsUtil;
-import com.bbva.rbvd.lib.r302.transform.bean.ModifyQuotationRimac;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.CollectionUtils;
 
-import java.util.Date;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Objects;
-
-import static com.bbva.rbvd.lib.r302.util.ConstantsUtil.EMAIL;
-import static com.bbva.rbvd.lib.r302.util.ConstantsUtil.MOBILE_NUMBER;
-import static com.bbva.rbvd.lib.r302.util.ConvertUtil.getGroupedByTypeContactDetail;
-import static com.bbva.rbvd.lib.r302.util.ValidationUtil.isBBVAClient;
 
 
 public class SimulationParticipantMap {
@@ -26,57 +14,30 @@ public class SimulationParticipantMap {
 
     private SimulationParticipantMap() {super();}
 
-    public static Map<String, Object> createArgumentsForSaveParticipant(SimulationParticipantDAO simulationParticipant, CustomerListASO customer){
+    public static Map<String, Object> createArgumentsForSaveParticipant(CommonsLifeDAO commonsLife){
         LOGGER.info("SimulationParticipantMap start - createArgumentsForSaveParticipant");
         Map<String, Object> arguments = new HashMap<>();
-        arguments.put(RBVDProperties.FIELD_INSURANCE_SIMULATION_ID.getValue(),simulationParticipant.getInsuranceSimulationId());
-        arguments.put(RBVDProperties.FIELD_OR_FILTER_INSURANCE_PRODUCT_ID.getValue(),simulationParticipant.getInsuranceProductId());
-        arguments.put(RBVDProperties.FIELD_INSURED_AMOUNT.getValue(),simulationParticipant.getResponse().getInsuredAmount().getAmount());
-        arguments.put(RBVDProperties.FIELD_PERIOD_TYPE.getValue(),"A");
-        arguments.put(RBVDProperties.FIELD_PERIOD_NUMBER.getValue(),simulationParticipant.getResponse().getTerm().getNumber());
-        arguments.put(RBVDProperties.FIELD_CUSTOMER_ENTRY_DATE.getValue(),new Date());
-
-        if(!CollectionUtils.isEmpty(simulationParticipant.getResponse().getListRefunds())){
-            arguments.put(RBVDProperties.FIELD_REFUND_PER.getValue(),simulationParticipant.getResponse().getListRefunds().get(0).getUnit().getPercentage());
-            arguments.put(RBVDProperties.FIELD_CURRENCY_ID.getValue(),simulationParticipant.getResponse().getListRefunds().get(1).getUnit().getCurrency());
-            arguments.put(RBVDProperties.FIELD_TOTAL_RETURN_AMOUNT.getValue(),simulationParticipant.getResponse().getListRefunds().get(1).getUnit().getAmount());
-        }
-        if(!CollectionUtils.isEmpty(simulationParticipant.getResponse().getParticipants())){
-            arguments.put(RBVDProperties.FIELD_INSURED_ID.getValue(),simulationParticipant.getResponse().getParticipants().get(0).getId());
-            arguments.put(RBVDProperties.FIELD_CUSTOMER_DOCUMENT_TYPE.getValue(),simulationParticipant.getResponse().getParticipants().get(0).getIdentityDocument().getDocumentType().getId());
-            arguments.put(RBVDProperties.FIELD_INSURED_CUSTOMER_NAME.getValue(),simulationParticipant.getResponse().getParticipants().get(0).getFirstName());
-            arguments.put(RBVDProperties.FIELD_CLIENT_LAST_NAME.getValue(),simulationParticipant.getResponse().getParticipants().get(0).getLastName());
-            arguments.put(RBVDProperties.FIELD_PHONE_ID.getValue(),simulationParticipant.getResponse().getParticipants().get(0).getContactDetails().get(0).getContact().getNumber());
-            arguments.put(RBVDProperties.FIELD_CUSTOMER_BIRTH_DATE.getValue(),simulationParticipant.getResponse().getParticipants().get(0).getBirthDate());
-            arguments.put(RBVDProperties.FIELD_PERSONAL_ID.getValue(),simulationParticipant.getResponse().getParticipants().get(0).getIdentityDocument().getDocumentNumber());
-            arguments.put(RBVDProperties.FIELD_IS_BBVA_CUSTOMER_TYPE.getValue(),isBBVAClient(simulationParticipant.getResponse().getParticipants().get(0).getId())? ConstantsUtil.YES_S : ConstantsUtil.NO_N);
-
-            if(StringUtils.isEmpty(simulationParticipant.getResponse().getParticipants().get(0).getId())){
-                arguments.put(RBVDProperties.FIELD_CUSTOMER_ENTRY_DATE.getValue(),null);
-            }
-            arguments.put(RBVDProperties.FIELD_USER_EMAIL_PERSONAL_DESC.getValue(), simulationParticipant.getResponse().getParticipants().get(0).getContactDetails().get(1).getContact().getAddress());
-
-        }else{
-            arguments.put(RBVDProperties.FIELD_INSURED_ID.getValue(),simulationParticipant.getResponse().getHolder().getId());
-            arguments.put(RBVDProperties.FIELD_CUSTOMER_DOCUMENT_TYPE.getValue(),simulationParticipant.getResponse().getHolder().getIdentityDocument().getDocumentType().getId());
-            arguments.put(RBVDProperties.FIELD_INSURED_CUSTOMER_NAME.getValue(),simulationParticipant.getResponse().getHolder().getFirstName());
-            arguments.put(RBVDProperties.FIELD_CLIENT_LAST_NAME.getValue(),simulationParticipant.getResponse().getHolder().getLastName());
-            arguments.put(RBVDProperties.FIELD_PERSONAL_ID.getValue(),simulationParticipant.getResponse().getHolder().getIdentityDocument().getDocumentNumber());
-            arguments.put(RBVDProperties.FIELD_USER_EMAIL_PERSONAL_DESC.getValue(),null);
-            arguments.put(RBVDProperties.FIELD_PHONE_ID.getValue(),null);
-            arguments.put(RBVDProperties.FIELD_CUSTOMER_BIRTH_DATE.getValue(),null);
-            arguments.put(RBVDProperties.FIELD_IS_BBVA_CUSTOMER_TYPE.getValue(),ConstantsUtil.YES_S);
-
-            if(Objects.nonNull(customer) && StringUtils.isNotEmpty(customer.getData().get(0).getBirthData().getBirthDate())){
-                arguments.put(RBVDProperties.FIELD_CUSTOMER_BIRTH_DATE.getValue(),ModifyQuotationRimac.ParseFecha(customer.getData().get(0).getBirthData().getBirthDate()));
-                Map<String, String> contactDetails = getGroupedByTypeContactDetail(customer.getData().get(0));
-                arguments.put(RBVDProperties.FIELD_USER_EMAIL_PERSONAL_DESC.getValue(),contactDetails.get(EMAIL));
-                arguments.put(RBVDProperties.FIELD_PHONE_ID.getValue(),contactDetails.get(MOBILE_NUMBER));
-            }
-        }
-        arguments.put(RBVDProperties.FIELD_CREATION_USER_ID.getValue(),simulationParticipant.getCreationUser());
-        arguments.put(RBVDProperties.FIELD_USER_AUDIT_ID.getValue(),simulationParticipant.getUserAudit());
-        arguments.put(RBVDProperties.FIELD_PARTICIPANT_ROLE_ID.getValue(),ConstantsUtil.IS_INSURED);
+        arguments.put(RBVDProperties.FIELD_INSURANCE_SIMULATION_ID.getValue(),commonsLife.getInsuranceSimulationId());
+        arguments.put(RBVDProperties.FIELD_OR_FILTER_INSURANCE_PRODUCT_ID.getValue(),commonsLife.getInsuranceProductId());
+        arguments.put(RBVDProperties.FIELD_INSURED_AMOUNT.getValue(),commonsLife.getInsuredAmount());
+        arguments.put(RBVDProperties.FIELD_PERIOD_TYPE.getValue(),commonsLife.getPeriodType());
+        arguments.put(RBVDProperties.FIELD_PERIOD_NUMBER.getValue(),commonsLife.getPeriodNumber());
+        arguments.put(RBVDProperties.FIELD_CUSTOMER_ENTRY_DATE.getValue(),commonsLife.getCustomerEntryDate());
+        arguments.put(RBVDProperties.FIELD_REFUND_PER.getValue(),commonsLife.getRefundPer());
+        arguments.put(RBVDProperties.FIELD_CURRENCY_ID.getValue(),commonsLife.getCurrencyId());
+        arguments.put(RBVDProperties.FIELD_TOTAL_RETURN_AMOUNT.getValue(),commonsLife.getTotalReturnAmount());
+        arguments.put(RBVDProperties.FIELD_INSURED_ID.getValue(),commonsLife.getInsuredId());
+        arguments.put(RBVDProperties.FIELD_CUSTOMER_DOCUMENT_TYPE.getValue(),commonsLife.getCustomerDocumentType());
+        arguments.put(RBVDProperties.FIELD_INSURED_CUSTOMER_NAME.getValue(),commonsLife.getInsuredCustomerName());
+        arguments.put(RBVDProperties.FIELD_CLIENT_LAST_NAME.getValue(),commonsLife.getClientLastName());
+        arguments.put(RBVDProperties.FIELD_PHONE_ID.getValue(),commonsLife.getPhoneId());
+        arguments.put(RBVDProperties.FIELD_CUSTOMER_BIRTH_DATE.getValue(),commonsLife.getCustomerBirthDate());
+        arguments.put(RBVDProperties.FIELD_PERSONAL_ID.getValue(),commonsLife.getPersonalId());
+        arguments.put(RBVDProperties.FIELD_IS_BBVA_CUSTOMER_TYPE.getValue(),commonsLife.getIsBbvaCustomerType());
+        arguments.put(RBVDProperties.FIELD_USER_EMAIL_PERSONAL_DESC.getValue(),commonsLife.getUserEmailPersonalDesc());
+        arguments.put(RBVDProperties.FIELD_CREATION_USER_ID.getValue(),commonsLife.getCreationUser());
+        arguments.put(RBVDProperties.FIELD_USER_AUDIT_ID.getValue(),commonsLife.getUserAudit());
+        arguments.put(RBVDProperties.FIELD_PARTICIPANT_ROLE_ID.getValue(),commonsLife.getParticipantRoleId());
         LOGGER.info("SimulationParticipantMap end");
         return arguments;
     }
