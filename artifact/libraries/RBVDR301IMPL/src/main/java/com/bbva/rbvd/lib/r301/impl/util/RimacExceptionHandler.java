5ea90754a579ffbedc961cca5f18a2c2aa30558a
paulkyev <paul.saire.contractor@bbva.com>
2023-11-13 11:24:05 -0500

    refact: PERAR-25613 PERAR-25656 change dependency error handler

--- artifact/libraries/RBVDR301IMPL/src/main/java/com/bbva/rbvd/lib/r301/impl/util/RimacExceptionHandler.java
+++ artifact/libraries/RBVDR301IMPL/src/main/java/com/bbva/rbvd/lib/r301/impl/util/RimacExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.bbva.rbvd.lib.r301.impl.util;
 
 import com.bbva.apx.exception.business.BusinessException;
-import com.bbva.rbvd.dto.insrncsale.bo.ErrorRimacBO;
-import com.bbva.rbvd.dto.insrncsale.utils.RBVDErrors;
-import com.bbva.rbvd.dto.insrncsale.utils.RBVDValidation;
+import com.bbva.rbvd.dto.insuranceroyal.utils.InsuranceRoyalErrors;
+import com.bbva.rbvd.dto.insuranceroyal.utils.InsuranceRoyalValidation;
+import com.bbva.rbvd.dto.insuranceroyal.rimac.ErrorRimacBO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.HttpClientErrorException;
@@ -19,7 +19,7 @@ public class RimacExceptionHandler {
             this.clientExceptionHandler((HttpClientErrorException) exception);
         } else {
             LOGGER.info("RimacExceptionHandler - HttpServerErrorException");
-            throw RBVDValidation.build(RBVDErrors.SERVER_ERROR);
+            throw InsuranceRoyalValidation.build(InsuranceRoyalErrors.SERVER_ERROR);
         }
     }
 
@@ -31,7 +31,7 @@ public class RimacExceptionHandler {
     }
 
     private void throwingBusinessException(ErrorRimacBO rimacError) {
-        BusinessException businessException = RBVDValidation.build(RBVDErrors.ERROR_FROM_RIMAC);
+        BusinessException businessException = InsuranceRoyalValidation.build(InsuranceRoyalErrors.ERROR_FROM_RIMAC);
         businessException.setMessage(rimacError.getError().getHttpStatus() + " - " + rimacError.getError().getMessage());
         throw businessException;
     }
