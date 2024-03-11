fb6bd534c2fd6392c9f78045543d2220c2dc4a5a
paulkyev <paul.saire.contractor@bbva.com>
2024-03-11 07:54:06 -0500

    deeegrgrg

--- artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/impl/RBVDR302Impl.java
+++ artifact/libraries/RBVDR302IMPL/src/main/java/com/bbva/rbvd/lib/r302/impl/RBVDR302Impl.java
@@ -1,8 +1,5 @@
 package com.bbva.rbvd.lib.r302.impl;
 
-import com.bbva.rbvd.dto.insuranceroyal.error.DetailsErrorDTO;
-import com.bbva.rbvd.dto.insuranceroyal.error.ErrorRequestDTO;
-import com.bbva.rbvd.dto.insuranceroyal.error.ErrorResponseDTO;
 import com.bbva.rbvd.dto.lifeinsrc.simulation.LifeSimulationDTO;
 import com.bbva.rbvd.lib.r302.pattern.Simulation;
 import com.bbva.rbvd.lib.r302.pattern.product.SimulationEasyYes;
@@ -12,10 +9,6 @@ import com.bbva.rbvd.lib.r302.pattern.product.SimulationVidaDinamico;
 import com.bbva.rbvd.lib.r302.util.ConstantsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class RBVDR302Impl extends RBVDR302Abstract {
 
@@ -46,33 +39,6 @@ public class RBVDR302Impl extends RBVDR302Abstract {
 					.withRbvdr044(this.rbvdR044 )
 					.build();
 			LOGGER.info("***** RBVDR302Impl - SimulationVidaDinamico ***** {}", simulation);
-			LOGGER.info("***** RBVDR302Impl - executeFindError START ******");
-
-			ErrorRequestDTO errorRequest = new ErrorRequestDTO();
-			List<DetailsErrorDTO> listDetailsError = new ArrayList<>();
-			DetailsErrorDTO detailsError = new DetailsErrorDTO();
-			detailsError.setCode("PER005005");
-			detailsError.setValue("El campo nroDocumento de persona en su elemento 1 debe contener 8 caracteres");
-			DetailsErrorDTO detailsError1 = new DetailsErrorDTO();
-			detailsError1.setCode("PER005011");
-			detailsError1.setValue("El campo nroDocumento de persona en su elemento 1 con valor \\\"@\\\" no coincide para el patrón: 0-9");
-			DetailsErrorDTO detailsError2 = new DetailsErrorDTO();
-			detailsError2.setCode("PER005003");
-			detailsError2.setValue("El campo nroDocumento de persona en su elemento 1 debe contener al menos 5 caracteres");
-			listDetailsError.add(detailsError);
-			listDetailsError.add(detailsError1);
-			listDetailsError.add(detailsError2);
-			errorRequest.setDetails(listDetailsError);
-			errorRequest.setTypeErrorScope("RIMAC");
-			errorRequest.setHttpCode(400L);
-			errorRequest.setCode("CP09");
-			List<ErrorResponseDTO> errorResponse = this.pisdR403.executeFindError(errorRequest);
-			LOGGER.info("***** RBVDR302Impl - executeFindError ***** {}", errorResponse);
-
-			LOGGER.info("***** RBVDR302Impl - executeFindError END ******");
-			for (ErrorResponseDTO err:errorResponse) {
-				this.addAdviceWithDescription(err.getCode(), err.getMessage());
-			}
 
 			response = simulation.start(input, this.rbvdR301, this.applicationConfigurationService);
 		}
